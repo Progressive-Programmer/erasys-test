ef75d5901af50608124c7f30ae49620770403d30
Aman <aman.singh@subcodevs.com>
2021-07-07 16:56:10 +0530

    ApiService.js: api services added

---
+++ src/Services/ApiService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+export const apiService = {
+    searchUsers: (length) => {
+        return axios.get(`/api/search?length=${length}`)
+    },
+    searchUserWithSorting: (length, sort) => {
+        return axios.get(`/api/search?length=${length}&sorting=${sort}`) //sorting=DISTANCE|ACTIVITY
+    },
+    getProfilesById: (id) => {
+        return axios.get(`/api/profiles?ids=${id}`) //profiles?ids=1111&ids=2222&...
+    },
+}

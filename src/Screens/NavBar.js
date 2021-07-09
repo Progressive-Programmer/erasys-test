f6175abde41e305cc03458107c6c206ed8cfbcb9
Aman <aman.singh@subcodevs.com>
2021-07-09 20:05:12 +0530

    React Context API setup - StateProvider.js

--- src/Screens/NavBar.js
+++ src/Screens/NavBar.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 import romeo_logo from '../Assets/romeo_logo2.svg'
+import { apiService } from '../Services/ApiService';
 
 export const NavBar =()=> {
+    const [length, setLength] = useState(39);
+    const [users, setUsers] = useState([])
+
+    const handleSearch = (e) => {
+        const name = e.target.name;
+    }
+
     return (
         <div className="topbar">
             <header className="header">
@@ -25,13 +33,13 @@ export const NavBar =()=> {
             <div className="navbarBig">
                 <ul>
                     <li className="navbarList">                     
-                        <a className="navbarOptions" >Discover</a>
+                        <a className="navbarOptions" name="discover" onClick={handleSearch} >Discover</a>
                     </li>
                     <li>                     
-                        <a className="navbarOptions" >Distance</a>
+                        <a className="navbarOptions" name="distance" onClick={handleSearch}  >Distance</a>
                     </li>
                     <li>                     
-                        <a className="navbarOptions" >Activity</a>
+                        <a className="navbarOptions" name="activity" onClick={handleSearch} >Activity</a>
                     </li>
                 </ul>
             </div>
